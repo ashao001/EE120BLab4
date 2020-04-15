6b7355677e5fd0c54f1c752418c0ed70a2a7bb39
Astraea Shao <ashao001@wch136-17.cs.ucr.edu>
2020-04-15 15:24:28 -0700

    uploaded p1

--- source/main.c
+++ source/main.c
@@ -12,12 +12,44 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {Start, Off, On} state;
+void Tick() {
+	unsigned char button = PINA & 0x01;
+	switch(state) { // Transitions
+    		case Start:    
+			state = Off; 
+			break;
+    		case Off:    
+			state = button ? On : Off; 
+			break;
+    		case On:    
+			state = button ? Off : On;
+			break;
+    		default:    
+			state = Start; 
+			break;
+    }; // Transitions
+
+	switch(state) { // State actions
+       		case Start:    
+			break;
+    		case Off:    
+			PORTB = 0x01; 
+			break;	
+    		case On:    
+			PORTB = 0x02;
+			break;
+    		default:    
+			break;
+       }; // State actions
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	state = Start;
     /* Insert your solution below */
     while (1) {
-
+	Tick();
     }
     return 1;
 }
